1a562b97a858e476fc27db23f4418c5849b678d5
becklime <becklime@gmail.com>
2018-11-28 22:41:06 +0300

    шел 11 час ночи

--- tile_server/static/index.js
+++ tile_server/static/index.js
@@ -2,14 +2,20 @@ alert('hi');
  var map = new ol.Map({
     target: 'map',
     layers: [
+
+      new ol.layer.Tile({
+        source: new ol.source.OSM({
+        })
+      }),
       new ol.layer.Tile({
         source: new ol.source.XYZ({
-            url: 'http://localhost:8000/myview/xyz/1.0.0/land/{z}/{x}/{y}.png'
+            url: 'http://localhost:8000/myview/xyz/1.0.0/land/{z}/{x}/{y}.png',
+            projection: 'EPSG:4326'
         })
       })
     ],
     view: new ol.View({
-      center: ol.proj.fromLonLat([37.41, 8.82]),
-      zoom: 4
+      center: ol.proj.fromLonLat([37.41, 44.82]),
+      zoom: 1
     })
   });
--- tile_server/utils.py
+++ tile_server/utils.py
@@ -1,7 +1,7 @@
 import pdb
 
 import mapnik
-
+import math
 
 # # Размеры тайла
 # TILE_WIDTH = 256
@@ -17,26 +17,46 @@ import mapnik
 # # Каталог для хранения кэша тайлов
 # CACHE_PATH = 'tile_server/tiles'
 
+def deg2num(lat_deg, lon_deg, zoom):
+    lat_rad = math.radians(lat_deg)
+    n = 2.0 ** zoom
+    xtile = int((lon_deg + 180.0) / 360.0 * n)
+    ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
+    return (xtile, ytile, xtile+1, ytile+1)
+
+
 def tms(z, x, y, service):
     # (4370311.220000, 4911352.860000) - (4403732.650000, 4958349.910000)
     # Extent: (4370177.610000, 4908530.650000) - (4411390.670000, 4958581.500000)
     #(38.376357, 44.940402) - (48.804400, 51.182006)
 
-    bbox = dict(minx=38, miny=44, maxx=50, maxy=53)
+    bbox = dict(minx=-180, miny=-90, maxx=180, maxy=90)
+    BBOX_MAXX=180
+    BBOX_MINX=-180
+    BBOX_MAXY=90
+    BBOX_MINY=-90
+    stepx = (BBOX_MAXX - 1.0 - BBOX_MINX) / (2 ** z)
+    stepy = (BBOX_MAXY - 1.0 - BBOX_MINY) / (2 ** z)
 
     step = max(bbox['maxx'] - bbox['minx'], bbox['maxy'] - bbox['miny']) / 2 ** z
 
     extents = dict()
 
     extents['tms'] = (
-        bbox['minx'] + x * step, bbox['miny'] + y * step,
-        bbox['minx'] + (x + 1) * step, bbox['miny'] + (y + 1) * step
+        bbox['minx'] + x * step,
+        bbox['miny'] + y * step,
+        bbox['minx'] + (x + 1) * step,
+        bbox['miny'] + (y + 1) * step
     )
 
     extents['xyz'] = (
-        bbox['minx'] + x * step, bbox['maxy'] - (y + 1) * step,
-        bbox['minx'] + (x + 1) * step, bbox['maxy'] - y * step
+        bbox['minx'] + x * step,
+        bbox['maxy'] - (y + 1) * step,
+        bbox['minx'] + (x + 1) * step,
+        bbox['maxy'] - y * step
     )
+
+    # extents['xyz'] = deg2num(x,y,z)
 
     tile = dict(width=256, height=256)
     map = mapnik.Map(tile['width'], tile['height'])
@@ -44,30 +64,31 @@ def tms(z, x, y, service):
 
     # mapnik.load_map(map, 'tile_server/style/styles.xml')
     mapnik.load_map(map, 'tile_server/style/mrsk.xml')
-    layer = mapnik.Layer('point')
-    ds = mapnik.PostGIS(host='127.0.0.1',
-                        dbname='isogd_sevastopol',
-                        user='postgres',
-                        password='qwerty12+',
-                        table='tableapi.table_test_2_points_2')
-    layer.datasource= ds
+    # layer = mapnik.Layer('point')
+    # ds = mapnik.PostGIS(host='127.0.0.1',
+    #                     dbname='isogd_sevastopol',
+    #                     user='postgres',
+    #                     password='qwerty12+',
+    #                     table='tableapi.table_test_2_points_2')
+    # layer.datasource= ds
+    # # pdb.set_trace()
+    # style= mapnik.Style()
+    # rule = mapnik.Rule()
+    # point_symbolizer = mapnik.PointSymbolizer()
+    # point_symbolizer.file= "/style/point_style.png"
+    # # pdb.set_trace()
+    #
+    # rule.symbols.append(point_symbolizer)
+    # style.rules.append(rule)
+    # map.append_style('My Style', style)
+
+
+
+    # map.zoom_all()
+    # mapnik.render_to_file(map, 'altay.png', 'png')
     # pdb.set_trace()
-    style= mapnik.Style()
-    rule = mapnik.Rule()
-    point_symbolizer = mapnik.PointSymbolizer()
-    point_symbolizer.file= "/style/point_style.png"
-    # pdb.set_trace()
-
-    rule.symbols.append(point_symbolizer)
-    style.rules.append(rule)
-    map.append_style('My Style', style)
-
-
-
-    map.zoom_all()
-    mapnik.render_to_file(map, 'altay.png', 'png')
-
     box = mapnik.Box2d(*extents.get(service))
+
     # map.zoom_all()
     map.zoom_to_box(box)
     mapnik.render_to_file(map, 'world.png', 'png')
